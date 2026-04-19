5f998dfb7fc6101959d1cd6c0a094df06ea139ac
nguyenquanganh102006-beep <nguyenquanganh102006@gmail.com>
2026-04-19 11:17:42 +0700

    Add files via upload

---
+++ tìm đường tàu_backend_3/backend/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "postgresql://postgres:1234@localhost:5432/tokyo_subway_"
+    SECRET_KEY: str = "your-secret-key-change-this-in-production"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 ngày
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
